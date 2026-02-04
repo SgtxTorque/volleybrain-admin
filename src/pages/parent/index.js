f2855d0396ba2b3a9e0f069740e58377c1dfd983
Carlos Fuentez <fuentez.carlos@gmail.com>
2026-02-04 00:49:46 -0600

    Update index.js
    Update 2.4.26

--- src/pages/parent/index.js
+++ src/pages/parent/index.js
@@ -1,4 +1,5 @@
 export { PlayerProfilePage } from './PlayerProfilePage'
+export { ParentPlayerCardPage } from './ParentPlayerCardPage'
 export { ParentMessagesPage } from './ParentMessagesPage'
 export { InviteFriendsPage } from './InviteFriendsPage'
 export { ParentPaymentsPage } from './ParentPaymentsPage'
